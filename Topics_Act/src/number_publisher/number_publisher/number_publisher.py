03b97b84f3ca71b4166f8497c37571a2343fc1e1
Shass27 <shaswaths@icloud.com>
2025-12-05 03:16:33 +0530

    Publisher node written, executable created

---
+++ Topics_Act/src/number_publisher/number_publisher/number_publisher.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import Int64
+
+class NumberPublisherNode(Node):
+    def __init__(self):
+        super().__init__("robot_news_station")
+        self.publisher_=self.create_publisher(Int64, "number", 10)
+        self.number = 0
+        self.timer_ = self.create_timer(1, self.publish_number)
+        self.get_logger().info("Number Publisher started")
+
+    def publish_number(self):
+        msg = Int64()
+        msg.data = self.number
+        self.publisher_.publish(msg)
+        self.number+=2
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = NumberPublisherNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
--- Topics_Act/src/number_publisher/setup.py
+++ Topics_Act/src/number_publisher/setup.py
@@ -24,6 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            "number_publisher = number_publisher.number_publisher:main"
         ],
     },
 )
